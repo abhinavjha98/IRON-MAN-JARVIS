fdaa18e0eda4adf57d0af8869150e555723179c0
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-04-10 19:01:04 +0530

    Added the command to jarvis

--- main.py
+++ main.py
@@ -26,38 +26,38 @@ engine.say("Thanks Shardul ")
 engine.say("Leader")
 engine.runAndWait()
 
-# def speak_test_cmd(cmd):
-# 	engine.say(cmd)
-# 	engine.runAndWait()
+def speak_test_cmd(cmd):
+	engine.say(cmd)
+	engine.runAndWait()
 
-# def read_voice_cmd():
-# 	voice_text = ""
-# 	print("Listening..")
-# 	with sr.Microphone() as source:
-# 		audio = speech.listen(source)
-# 	try:
-# 		voice_text = speech.recognize_google(audio)
-# 	except sr.UnknownValueError:
-# 		pass
-# 	except sr.RequestError as e:
-# 		print("Network Problem")
-# 	return voice_text
+def read_voice_cmd():
+	voice_text = ""
+	print("Listening..")
+	with sr.Microphone() as source:
+		audio = speech.listen(source)
+	try:
+		voice_text = speech.recognize_google(audio)
+	except sr.UnknownValueError:
+		pass
+	except sr.RequestError as e:
+		print("Network Problem")
+	return voice_text
 
-# if __name__ == '__main__':
-# 	speak_test_cmd("Hello Mr.Abhinav This is Jarvis Your Artificial Intelligence")
+if __name__ == '__main__':
+	speak_test_cmd("Hello Mr.Abhinav This is Jarvis Your Artificial Intelligence")
 
-# 	while True:
-# 		voice_note = read_voice_cmd()
-# 		print('cmd: {}'.format(voice_note))
-# 		if 'hello' in voice_note:
-# 			speak_test_cmd("Hello Abhinav How I can help you?")
-# 			continue
-# 		elif 'open' in voice_note:
-# 			os.system('explorer C:\\ {}'.format(voice_note.replace('Open ','')))
-# 			continue
-# 		elif 'bye' in voice_note:
-# 			speak_test_cmd("Bye Abhinav Happy to help you.Have a good day.")
-# 			exit()	
+	while True:
+		voice_note = read_voice_cmd()
+		print('cmd: {}'.format(voice_note))
+		if 'hello' in voice_note:
+			speak_test_cmd("Hello Abhinav How I can help you?")
+			continue
+		elif 'open' in voice_note:
+			os.system('explorer C:\\ {}'.format(voice_note.replace('Open ','')))
+			continue
+		elif 'bye' in voice_note:
+			speak_test_cmd("Bye Abhinav Happy to help you.Have a good day.")
+			exit()	
 
 
 
